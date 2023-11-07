940ced030537e4d9b7d1b01998d4c9b592004538
ElyesBradai <123512364+ElyesBradai@users.noreply.github.com>
2023-11-07 16:33:54 -0500

    Add files via upload

---
+++ NeuralNetwork.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+public class NeuralNetwork {
+
+    private int generation;
+    private HiddenLayer[] hiddenLayers;
+    private double[][] activations;
+    private int mutationCount;
+
+    // layers[0] = input
+    // layers[].length-1 = output
+    // the rest is all hidden layers
+
+
+    public NeuralNetwork(int[]... layers) {
+
+
+
+
+    }
+
+
+
+
+}
